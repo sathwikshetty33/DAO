0b369d6375ab1958520c4d9838f7684a4b8ff737
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2025-01-21 01:08:25 +0530

    aithu broo

--- contracts/twitter.sol
+++ contracts/twitter.sol
@@ -25,6 +25,7 @@ contract twitter{
     uint nextMessageId;
     function _tweet(address _from, string memory _content) internal {
         tweets[nextId]=Tweet({id:nextId,author:_from,content:_content,createdAt:block.timestamp});
+        tweetsof[_from].push(nextId);
         nextId++;
     }
     function _message(address _from, address _to, string memory _content) public {
@@ -43,4 +44,35 @@ contract twitter{
     function message(address _from, address _to, string memory _content) public {
         _message(_from, _to, _content);
     }
+    function follow(address _tofollow) public {
+        following[msg.sender].push(_tofollow);
+    }
+    function allowaccess(address _user) public{
+        operators[msg.sender][_user]=true;
+    }
+    function removeaccess(address _user) public{
+        operators[msg.sender][_user]=false;
+    }
+    function getLatesTweets(uint count)public view returns(Tweet[] memory) {
+        require(count >0 && count < nextId,"Wrong input");
+        Tweet[] memory _tweets=new Tweet[](count);
+        uint j;
+        for(uint i =nextId-count;i<nextId;i++)
+        {
+            _tweets[j]= tweets[i];
+            j ++ ;
+        }
+        return _tweets;
+    }
+    function getLatestTweetsOfUser(address _user, uint count) public view returns (Tweet[] memory) {
+    uint[] memory indexes = tweetsof[_user];
+    require(count > 0 && count <= indexes.length, "Invalid count");
+
+    Tweet[] memory _tweets = new Tweet[](count);
+    uint start = indexes.length - count;
+    for (uint i = 0; i < count; i++) {
+        _tweets[i] = tweets[indexes[start + i]];
+    }
+    return _tweets;
+}
 }
